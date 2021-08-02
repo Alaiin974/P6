8154ceac8d009191f8c5997f1014cd28ba4f56a5
Alaiin974 <clain_alain97423@outlook.fr>
2021-08-02 13:42:21 +0400

    TEST v2

--- lamp_wp_2.py
+++ lamp_wp_2.py
@@ -4,6 +4,47 @@
 import os
 import time
 
+##### VARIABLES #####
+
+a2dir = ("sed -i 's/html//g' /etc/apache2/sites-enabled/000-default.conf")
+
+wpdir = '/var/www/' 
+wpdirname = raw_input ("Wordpress Directory Name ? " )
+wpdirname1 = 'mv wordpress ' + '/var/www/' + wpdirname
+chwpdir = 'chmod -R 755 /var/www/' + wpdirname + ' && chown -R www-data:www-data /var/www/' + wpdirname
+
+vhconfname = wpdirname + ".conf"
+vhconfcp = 'cp virtualhost.conf /etc/apache2/sites-available/' + vhconfname
+servername = raw_input ("Server Name ? ")
+vhservername = "sed -i 's/wp.default/" + servername + "/g' /etc/apache2/sites-available/" + vhconfname
+vhdocroot = "sed -i 's/wpdefault/" + wpdirname + "/g' /etc/apache2/sites-available/" + vhconfname
+
+hosts = "sed -i '1i\127.0.1.1       " + servername + " \n' /etc/hosts"
+
+hostname = "sed -i 's/.*/" + wpdirname + "/g' /etc/hostname"
+
+a2enre = 'a2enmod rewrite && a2ensite ' + vhconfname + ' && systemctl restart apache2'
+
+dbname = raw_input ("Database Name ? ")
+dbname2 = "mysql -u root -e " + '"create database ' + dbname +  '"'
+
+dbusername = raw_input ("Database User ? ")
+dbpassword = raw_input ("Database User Password ? ")
+
+dbuser = "mysql -u root -e " + '"grant all privileges on *.* to ' + dbusername + '@localhost' + " identified by " + "'" + dbpassword + "'" + '"'
+
+dbconn = "mv /var/www/" + wpdirname + "/wp-config-sample.php /var/www/" + wpdirname + "/wp-config.php"
+dbconn2 = "sed -i 's/votre_nom_de_bdd/" + dbname + "/g' /var/www/" + wpdirname + "/wp-config.php"
+dbconn3 = "sed -i 's/votre_utilisateur_de_bdd/" + dbusername + "/g' /var/www/" + wpdirname + "/wp-config.php"
+dbconn4 = "sed -i 's/votre_mdp_de_bdd/" + dbpassword + "/g' /var/www/" + wpdirname + "/wp-config.php"
+
+##### SET TIMEZONE #####
+
+print ("----- SET TIMEZONE -----")
+time.sleep(2)
+
+os.system("dpkg-reconfigure tzdata")
+
 ##### PACKAGES INSTALLATON #####
 
 print ("----- PACKAGES INSTALLATON : APACHE2, MARIADB-SERVER, PHP, WGET -----")
@@ -27,8 +68,6 @@ os.system ('tar xvf latest.tar.gz')
 print ("----- SETTING DEFAULT APACHE DIRECTORY -----")
 time.sleep(2)
 
-a2dir = ("sed -i 's/html//g' /etc/apache2/sites-enabled/000-default.conf")
-
 os.system(a2dir)
 os.system('rm -r /var/www/html')
 
@@ -37,11 +76,6 @@ os.system('rm -r /var/www/html')
 print ("----- SET WORDPRESS DIRECTORY -----")
 time.sleep(2)
 
-wpdir = '/var/www/' 
-wpdirname = raw_input ("Wordpress Directory Name ? " )
-wpdirname1 = 'mv wordpress ' + '/var/www/' + wpdirname
-chwpdir = 'chmod -R 755 /var/www/' + wpdirname + ' && chown -R www-data:www-data /var/www/' + wpdirname 
-
 os.system(wpdirname1)
 os.system(chwpdir)
 
@@ -49,12 +83,6 @@ os.system(chwpdir)
 
 print ("----- SET VIRTUALHOST -----")
 time.sleep(2)
-
-vhconfname = wpdirname + ".conf"
-vhconfcp = 'cp virtualhost.conf /etc/apache2/sites-available/' + vhconfname
-servername = raw_input ("Server Name ? ")
-vhservername = "sed -i 's/wp.default/" + servername + "/g' /etc/apache2/sites-available/" + vhconfname
-vhdocroot = "sed -i 's/wpdefault/" + wpdirname + "/g' /etc/apache2/sites-available/" + vhconfname
 
 os.system(vhconfcp)
 os.system(vhservername)
@@ -65,16 +93,12 @@ os.system(vhdocroot)
 print ("----- SETTING /ETC/HOSTS -----")
 time.sleep(2)
 
-hosts = "sed -i '1i\127.0.1.1       " + servername + " \n' /etc/hosts"
-
 os.system(hosts)
 
 ##### SET HOSTNAME #####
 
 print ("----- SETTING HOSTNAME -----")
 time.sleep(2)
-
-hostname = "sed -i 's/.*/" + wpdirname + "/g' /etc/hostname"
 
 os.system(hostname)
 
@@ -83,32 +107,14 @@ os.system(hostname)
 print ("----- ENABLE SITE  & RESTART APACHE -----")
 time.sleep(2)
 
-a2enre = 'a2enmod rewrite && a2ensite ' + vhconfname + ' && systemctl restart apache2'
-
 os.system(a2enre)
-
-##### SET TIMEZONE #####
-
-print ("----- SET TIMEZONE -----")
-time.sleep(2)
-
-os.system("dpkg-reconfigure tzdata")
 
 ##### DATABASE #####
 
 print ("-----SET WORDPRESS DATABASE -----")
 time.sleep(2)
 
-dbname = raw_input ("Database Name ? ")
-dbname2 = "mysql -u root -e " + '"create database ' + dbname +  '"'
-
 os.system(dbname2)
-
-dbusername = raw_input ("Database User ? ")
-dbpassword = raw_input ("Database User Password ? ")
-
-dbuser = "mysql -u root -e " + '"grant all privileges on *.* to ' + dbusername + '@localhost' + " identified by " + "'" + dbpassword + "'" + '"'
-
 os.system(dbuser)
 os.system("systemctl restart mariadb")
 
@@ -116,11 +122,6 @@ os.system("systemctl restart mariadb")
 
 print ("----- SETTING DATABASE CONNECTION -----")
 time.sleep(2)
-
-dbconn = "mv /var/www/" + wpdirname + "/wp-config-sample.php /var/www/" + wpdirname + "/wp-config.php"
-dbconn2 = "sed -i 's/votre_nom_de_bdd/" + dbname + "/g' /var/www/" + wpdirname + "/wp-config.php"
-dbconn3 = "sed -i 's/votre_utilisateur_de_bdd/" + dbusername + "/g' /var/www/" + wpdirname + "/wp-config.php"
-dbconn4 = "sed -i 's/votre_mdp_de_bdd/" + dbpassword + "/g' /var/www/" + wpdirname + "/wp-config.php"
 
 os.system (dbconn)
 os.system (dbconn2)
