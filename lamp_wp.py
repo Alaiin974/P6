869f87b6858b1056922557d701b1031201fd5fc9
Alaiin974 <clain_alain97423@outlook.fr>
2021-08-02 11:33:23 +0400

    MAJ

--- lamp_wp.py
+++ lamp_wp.py
@@ -6,21 +6,20 @@ import time
 
 ##### PACKAGES INSTALLATON #####
 
-print ("----- PACKAGES INSTALLATON : APACHE2, MARIADB-SERVER, PHP, WGET, GIT -----")
+print ("----- PACKAGES INSTALLATON : APACHE2, MARIADB-SERVER, PHP, WGET -----")
 time.sleep(2)
 
 os.system('apt install apache2 -y')
 os.system('apt install mariadb-server -y')
 os.system('apt install php7.3 php7.3-mysql -y')
 os.system('apt install wget -y')
-os.system('apt install git -y')
 
 ##### LATEST WORDPRESS ARCHIVE #####
 
 print ("----- DOWNLOAD AND UNCOMPRESS LATEST WORDPRESS ARCHIVE -----")
 time.sleep(2)
 
-os.system('wget https://wordpress.org/latest.tar.gz')
+#os.system('wget https://wordpress.org/latest.tar.gz')
 os.system ('tar xvf latest.tar.gz')
 
 ##### /VAR/WWW/HTML & APACHE CONF #####
--- letsencrypt.py
+++ letsencrypt.py
@@ -4,9 +4,22 @@
 import os
 import time
 
-os.system("apt install sudo snap")
+##### https://certbot.eff.org/lets-encrypt/debianbuster-apache #####
+
+print ("----- PACKAGES INSTALLATION : SUDO, SNAPD -----")
+os.system("apt install sudo snapd")
+
+print ("----- SNAPD LATEST VERSION CHECK -----")
 os.system("sudo snap install core; sudo snap refresh core")
+
+print ("----- INSTALL CERTBOT -----")
 os.system("sudo snap install --classic certbot")
+
+print ("----- ENABLE CERTBOT COMMAND -----")
 os.system("sudo ln -s /snap/bin/certbot /usr/bin/certbot")
+
+print ("----- GENERATE SSL CERTIFICATE -----")
 os.system("sudo certbot --apache")
+
+print ("----- ENABLE AUTOMATIC SSL CERTIFICATE RENEWAL -----")
 os.system("sudo certbot renew --dry-run")
